102655c502546482ec8c84460b44178210ce6f08
Jason Stevens <jason@fivegrain.com>
2025-12-03 15:17:42 -0500

    day03 working

---
+++ day03/day03.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+
+def get_line_joltage(digits: list[int]) -> int:
+    # import pdb; pdb.set_trace()
+    first_digit_i = 0
+    second_digit_i = 1
+    final_i = len(digits) - 1
+    for i in range(1, len(digits)):
+        if i < final_i and digits[i] > digits[first_digit_i]:
+            first_digit_i = i
+            second_digit_i = i + 1
+        elif digits[i] > digits[second_digit_i]:
+            second_digit_i = i
+    result = (10 * digits[first_digit_i]) + digits[second_digit_i]
+    # print(f'{line} - {result}')
+    return result
+
+def index_of_max(digits: list[int]) -> int:
+    max_i = 0
+    for i in range(1, len(digits)):
+        if digits[i] > digits[max_i]:
+            max_i = i
+    # print(f'Picking {digits[max_i]} (index {max_i}) out of {digits}')
+    return max_i
+
+def get_fancy_line_joltage(digits: list[int], num_digits_needed: int = 12) -> int:
+    # Find the first largest digit within digits[:-num_digits_needed]
+    # Then append to that the result a recursively calling this with
+    # all the digits after the the one we pick and num_digits_needed-1
+    # print(f'get_fancy_line_joltage digits:{digits} num_digits_needed:{num_digits_needed}')
+    my_digit_i = index_of_max(digits if num_digits_needed == 1 else digits[:-(num_digits_needed-1)])
+    if num_digits_needed > 1:
+        joltage_to_right = get_fancy_line_joltage(digits[my_digit_i+1:], num_digits_needed-1)
+    else:
+        joltage_to_right = ''
+    result = int(str(digits[my_digit_i]) + str(joltage_to_right))
+    # print(f'Total {result}')
+    if len(str(result)) != num_digits_needed:
+        raise Exception('Whoa!')
+    return result
+
+def add_joltages(lines: list[str], calculator: callable) -> int:
+    total = 0
+    for line in lines:
+        digits = [int(c) for c in line]
+        total += calculator(digits)
+    return total
+
+for calculator in (('Part One', get_line_joltage), ('Part Two', get_fancy_line_joltage)):
+# for calculator in (('Part Two', get_fancy_line_joltage),):
+    print(calculator[0])
+    for input_file in ('test.txt', 'input.txt'):
+        with open(input_file, 'r') as file:
+            lines = [s for l in file.readlines() if (s := l.strip())]
+        total = add_joltages(lines, calculator[1])
+        print(f'For {input_file}:')
+        print(f'  total: {total}')
+    print()
+
