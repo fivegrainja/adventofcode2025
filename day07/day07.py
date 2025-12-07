f82633be53ee677841eb1766b27e292404b92a24
Jason Stevens <jason@fivegrain.com>
2025-12-07 01:36:06 -0500

    day 07 part two

--- day07/day07.py
+++ day07/day07.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import functools
+
 def part_one(lines: list[str]) -> int:
     beams = set([lines[0].index('S')])
     num_splits = 0
@@ -14,13 +16,31 @@ def part_one(lines: list[str]) -> int:
         beams = next_beams
     return num_splits
 
+@functools.lru_cache
+def count_splits(lines: tuple[str], beam_loc: int, line_num: int) -> int:
+    if line_num == len(lines):
+        return 0
+    if lines[line_num][beam_loc] == '.':
+        return count_splits(lines, beam_loc, line_num + 1)
+    if lines[line_num][beam_loc] == '^':
+        return (1 
+            + count_splits(lines, beam_loc - 1, line_num + 1)
+            + count_splits(lines, beam_loc + 1, line_num + 1))
+    raise Exception('Should not get here')
+
+def part_two(lines: tuple[str]) -> int:
+    beam_loc = lines[0].index('S')
+    result = 1 + count_splits(lines, beam_loc, 1)
+    return result
+
 if __name__ == '__main__':      
     for input_file in ('test.txt', 'input.txt'):
         with open(input_file, 'r') as file:
-            lines = [s for l in file.readlines() if (s := l.strip())]
+            # Use a tuple instead of list so that lru_cache can hash it
+            lines = tuple([s for l in file.readlines() if (s := l.strip())])
         part_one_result = part_one(lines)
         print(f'  Part one: {part_one_result}')
-        # part_two_result = part_two(lines)
-        # print(f'  Part two: {part_two_result}')
+        part_two_result = part_two(lines)
+        print(f'  Part two: {part_two_result}')
         print()
 
