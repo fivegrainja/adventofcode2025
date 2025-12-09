4009b87d91de764fe0993d4b4b623710a60ecab2
Jason Stevens <jason@fivegrain.com>
2025-12-09 16:14:40 -0500

    prep for day10

---
+++ day10/day10.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+# ]
+# ///
+
+from pathlib import Path
+
+def part_one(lines: list[str]) -> int:
+    return None
+
+def part_two(lines: list[str]) -> int:
+    return None
+
+if __name__ == '__main__':      
+    for input_file in ('test.txt',): # 'input.txt'):
+        print(f'{input_file}:')
+        with open(Path(__file__).resolve().parent / input_file, 'r') as file:
+            lines = [s for l in file.readlines() if (s := l.strip())]
+        result = part_one(lines)
+        print(f'  Part one: {result}')
+        result = part_two(lines)
+        print(f'  Part two: {result}')
+        print()
+        
